698688c7e783ba2f0eb5538290a4fe303c6a6616
jessica lorrane <jessicalorrane.asantaclara@gmail.com>
2022-08-23 10:20:26 -0300

    sim

---
+++ aula12ex/script.js
@@ -0,0 +1,17 @@
+    function verificar() {
+ var data = new Date ()
+ var ano = data.getFullYear ()
+ var fano = document.getElementById('txtano')
+ var res = document.querySelector('div#res ')
+ if ( fano.ariaValueMax.length ==0 || Number(f.ano.value) > ano) {
+    window.alert ('[erro] verifique os dados e tente novamengte !') {
+    }   else {
+        var fsex = document.getElementsByName ('radius')
+        var idade = ano Number(fano.value)
+        res.innerHTML = ano- Number (fano.value)
+        res.innerHTML = 'idde calculada : $(idade)'
+
+        }
+    }
+ }
+    }
